63834fbb2421506205745bb596ff8ac726361f2a
Andrey Grygoryev <undeadgrandse@gmail.com>
2012-06-15 09:42:55 +0300

    Add a manifest file and update a setup.py classifiers

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 Lighty-template
 ~~~~~~~~~~~~~~~
 
-Lighty-template is very simple template engine for python (python.org). 
-Template syntax looks like django-template or jinja2 template. But template 
-engine code is easier and gives a way to write all needed tags without any 
-hacks. 
+Lighty-template is very simple template engine for python (python.org).
+Template syntax looks like django-template or jinja2 template. But template
+engine code is easier and gives a way to write all needed tags without any
+hacks.
 
-Now it does not include all features django-template or jinja2 supports, but 
+Now it does not include all features django-template or jinja2 supports, but
 I'll try to fix it as soon as possible.
 
 Features:
@@ -23,7 +23,7 @@ Features:
 - Template filters with multiply arguments.
 - Basic template filters included (now just 14 template filters).
 - Basic template tags included.
-- Simple but powerfull tag declaration - it's easy to create your own block 
+- Simple but powerfull tag declaration - it's easy to create your own block
   tags with writing single function.
 - Custom template tags can modify template on fly.
 
@@ -32,28 +32,28 @@ Example:
 
 Here a small template example:
 
-	<!DOCTYPE html>
-	<html>
-	<head>
-		<title>{{ title }}</title>
-		{% block style %}{% endblock %}
-		{% block script %}{% endblock %}
-	</head>
-	<body>
-		{% block content %}
-		<h1>Hello, {{ name }}!</h1>
-		<p>Some text here</p>
-		{% endblock %}
-		{% include "includes/footer.html" %}
-	</body>
-	</html>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>{{ title }}</title>
+        {% block style %}{% endblock %}
+        {% block script %}{% endblock %}
+    </head>
+    <body>
+        {% block content %}
+        <h1>Hello, {{ name }}!</h1>
+        <p>Some text here</p>
+        {% endblock %}
+        {% include "includes/footer.html" %}
+    </body>
+    </html>
 
 TODO:
 -----
 
 - More default tags (now there is no load tags, and if and for tags was
   simplified and requires additional work).
-- More default filters (date formatiing, strings saving, etc.)
+- More default filters (strings saving, etc.)
 - Some additional execution optimizations.
 - More tests (in progress).
 - Documentation.
@@ -66,7 +66,7 @@ setup(
         version='0.3.4',
         description='Simple template engine for python',
         long_description=__doc__,
-        keywords='Template HTML XML', 
+        keywords='Template HTML XML',
         author='Andrey Grygoryev',
         author_email='undeadgrandse@gmail.com',
         license='BSD',
@@ -74,14 +74,18 @@ setup(
         packages=['lighty', 'lighty.templates'],
         platforms="any",
         classifiers=[
+            'Development Status :: 4 - Beta',
             'Environment :: Web Environment',
             'Intended Audience :: Developers',
             'License :: OSI Approved :: BSD License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
             'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'Topic :: Text Processing :: Markup :: HTML',
+            'Topic :: Text Processing :: Markup :: XML',
         ],
 )
